e5b7bab2a9855237d1eba7c7c5c0e32ee4e22e82
Alok Sahay <alok.sahay87@gmail.com>
2024-08-08 13:52:22 +0200

    Added iOS helpers

---
+++ MintCondition/MintCondition/Helpers/CastManager.swift
@@ -0,0 +1,135 @@
+//
+//  CastManager.swift
+
+import Foundation
+
+struct CastId: Codable {
+    let fid: Int
+    let hash: String
+}
+
+struct EmbedCast: Codable {
+    let castId: CastId
+}
+
+struct EmbedUrl: Codable {
+    let url: String
+}
+
+
+
+struct Cast: Codable {
+    let id: String
+    let castText: String
+    let embedUrl: [EmbedUrl]
+    let embedCast: [EmbedCast]
+    let username: String
+    let pfp: String
+    let timestamp: String
+    let likes: Int
+    let recasts: Int
+}
+
+struct PostBody: Codable {
+    let signer: String
+    let castMessage: String
+    let fid: String
+    let parentUrl: String
+}
+
+class CastManager {
+    static let shared = CastManager()
+    
+    var casts: [Cast] = []
+    
+    func fetchCasts(channel: String, completion: @escaping (Result<[Cast], Error>) -> Void) {
+        guard let url = URL(string: "https://swooop-server.onrender.com/feed?channel=\(channel)&pageToken=blank&api=true") else {
+            completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No data received", code: 1, userInfo: nil)))
+                return
+            }
+            
+            do {
+                let decodedData = try JSONDecoder().decode([Cast].self, from: data)
+                self.casts = decodedData
+                completion(.success(decodedData))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+    
+    func postCast(text: String, parentUrl: String) { 
+        var user_fid: String = ""
+        var user_signer: String = ""
+        if let fid = KeyValueStore.shared.value(forKey: "fid") as? String {
+            print("fid: \(fid)")
+            user_fid = fid
+        } else {
+            print("fid not found")
+        }
+        
+        if let signer_private = KeyValueStore.shared.value(forKey: "signer_private") as? String {
+            print("signer_private: \(signer_private)")
+            user_signer = signer_private
+        } else {
+            print("signer_private not found")
+        }
+        let url = URL(string: "http://192.168.1.103:3000/message")! //lo
+        
+        let postBody = PostBody(signer: user_signer,
+                                castMessage: text,
+                                fid: user_fid,
+                                parentUrl: parentUrl)
+
+        let jsonEncoder = JSONEncoder()
+        do {
+            let jsonData = try jsonEncoder.encode(postBody)
+            
+            if String(data: jsonData, encoding: .utf8) != nil {
+                
+                var request = URLRequest(url: url)
+
+                request.httpMethod = "POST"
+
+                request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+                request.httpBody = jsonData
+
+                let session = URLSession.shared
+
+                let task = session.dataTask(with: request) { data, response, error in
+                    if let error = error {
+                        print("Error: \(error)")
+                        return
+                    }
+                    
+                    guard let data = data else {
+                        print("No data returned")
+                        return
+                    }
+                    
+                    if let responseString = String(data: data, encoding: .utf8) {
+                        print("Response: \(responseString)")
+                    }
+                }
+
+                task.resume()
+
+            }
+        } catch {
+            print("Error encoding PostBody to JSON:", error)
+        }
+    }
+}
+
---
+++ MintCondition/MintCondition/Helpers/UserManager.swift
@@ -0,0 +1,210 @@
+//
+//  UserManager.swift
+
+import Foundation
+
+struct User: Codable {
+    let fid: Int
+    let username: String
+    let pfp: String
+}
+
+struct SignerPayload: Codable {
+    let pollingToken: String
+    let privateKey: String
+    let publicKey: String
+    let deepLinkUrl: String
+}
+
+struct Completion: Codable {
+    let state: String
+    let userFid: Int?
+}
+
+class UserManager {
+    static let shared = UserManager()
+    
+    func getUserData() -> User? {
+        var user_name: String = ""
+        var user_fid: String = ""
+        var user_pfp: String = "https://static.vecteezy.com/system/resources/thumbnails/004/511/281/small/default-avatar-photo-placeholder-profile-picture-vector.jpg"
+        if let username = KeyValueStore.shared.value(forKey: "username") as? String {
+            user_name = username
+        } else {
+            print("username not found")
+        }
+        
+        if let fid = KeyValueStore.shared.value(forKey: "fid") as? String {
+            user_fid = fid
+        } else {
+            print("fid not found")
+        }
+        
+        if let pfp = KeyValueStore.shared.value(forKey: "pfp") as? String {
+            user_pfp = pfp
+        } else {
+            print("pfp not found")
+        }
+        
+        if(user_fid != "" && user_pfp != "" && user_name != "") {
+            let newUser = User(fid: Int(user_fid)!, username: user_name, pfp: user_pfp)
+            return newUser
+        }
+        
+        return User(fid: 0, username: "", pfp: "")
+    }
+    
+    func fetchUser(fid: String, completion: @escaping (Result<User, Error>) -> Void) {
+        let urlString = "https://swooop-server.onrender.com/user?userFid=\(fid)"
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
+            return
+        }
+        
+        // Create the GET request
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        // Create URLSessionDataTask to make the request
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            // Check for error
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            // Check for response status code
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid response"])))
+                return
+            }
+            
+            // Check if data is available
+            guard let data = data else {
+                completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "No data received"])))
+                return
+            }
+            
+            do {
+                // Decode the JSON response into the User struct
+                let user = try JSONDecoder().decode(User.self, from: data)
+                completion(.success(user))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+    
+    func pollForSuccess(token: String, completion: @escaping (Result<Completion, Error>) -> Void) {
+        print("We polling...")
+        let urlString = "https://swooop-server.onrender.com/sign-in/poll?pollingToken=\(token)"
+        guard let url = URL(string: urlString) else {
+            completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid URL"])))
+            return
+        }
+        
+        // Create the GET request
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        
+        // Create URLSessionDataTask to make the request
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            // Check for error
+            if let error = error {
+                print("Main polling error")
+                completion(.failure(error))
+                return
+            }
+            
+            // Check for response status code
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                print("Non 200 response")
+                completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "Invalid response"])))
+                return
+            }
+            
+            // Check if data is available
+            guard let data = data else {
+                print("No data received")
+                completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "No data received"])))
+                return
+            }
+            
+            do {
+                let complete = try JSONDecoder().decode(Completion.self, from: data)
+                let status: String = complete.state
+                print("Status")
+                print(status)
+                if (status == "completed") {
+                    KeyValueStore.shared.setValue(String(complete.userFid ?? 0), forKey: "fid")
+                    self.fetchUser(fid: String(complete.userFid ?? 0)) { result in
+                        switch result {
+                        case .success(let user):
+                            print(user)
+                            KeyValueStore.shared.setValue(user.username, forKey: "username")
+                            KeyValueStore.shared.setValue(user.pfp, forKey: "pfp")
+                        case .failure(let error):
+                            // Handle error
+                            print("Error: \(error)")
+                        }
+                    }
+                    print("WE MADE IT")
+                    KeyValueStore.shared.setValue("true", forKey: "signer_approved")
+                    completion(.success(complete))
+                } else {
+                    print("We poll again")
+                    self.pollForSuccess(token: token, completion: completion)
+                }
+            } catch {
+                print("Catch failure in main polling")
+                print(error);
+                completion(.failure(error))
+            }
+        }.resume()
+    }
+    
+    func signIn(completion: @escaping (Result<SignerPayload, Error>) -> Void) {
+        print("Signing in...")
+        let url = URL(string: "https://swooop-server.onrender.com/sign-in")!
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            // Check for HTTP response status code indicating success
+            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+                let statusCode = (response as? HTTPURLResponse)?.statusCode ?? -1
+                completion(.failure(NSError(domain: "", code: statusCode, userInfo: [NSLocalizedDescriptionKey: "Unexpected response"])))
+                return
+            }
+            
+            // Ensure data is present
+            guard let responseData = data else {
+                completion(.failure(NSError(domain: "", code: 0, userInfo: [NSLocalizedDescriptionKey: "No data received"])))
+                return
+            }
+            
+            do {
+                // Decode the JSON response into a SignerPayload object
+                let decoder = JSONDecoder()
+                let signerPayload = try decoder.decode(SignerPayload.self, from: responseData)
+                print("Signer payload")
+                print(signerPayload)
+                KeyValueStore.shared.setValue(signerPayload.privateKey, forKey: "signer_private")
+                KeyValueStore.shared.setValue("false", forKey: "signer_approved")
+                KeyValueStore.shared.setValue(signerPayload.pollingToken, forKey: "polling_token")
+                completion(.success(signerPayload))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        task.resume()
+    }
+}
+
