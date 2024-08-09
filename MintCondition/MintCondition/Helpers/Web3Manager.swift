f22dea86fa5ea4fc12651f4f093512c71c586814
Alok Sahay <alok.sahay87@gmail.com>
2024-08-09 12:28:52 +0200

    Added networks

--- MintCondition/MintCondition/Helpers/Web3Manager.swift
+++ MintCondition/MintCondition/Helpers/Web3Manager.swift
@@ -9,6 +9,37 @@ import Foundation
 import Web3
 import BigInt
 
+struct Network {
+    
+    let name: String
+    let rpcURL: String
+    let explorerURL: String
+    
+    static let baseTestnet = Network(
+        name: "Base Sepolia",
+        rpcURL: "https://base-sepolia.g.alchemy.com/v2/",
+        explorerURL: "https://basescan.org"
+    )
+    
+    static let baseMainnet = Network(
+        name: "Base",
+        rpcURL: "https://base-mainnet.g.alchemy.com/v2/",
+        explorerURL: "https://basescan.org"
+    )
+    
+    static let optimism = Network(
+        name: "Optimism",
+        rpcURL: "https://opt-mainnet.g.alchemy.com/v2/",
+        explorerURL: "https://optimistic.etherscan.io"
+    )
+    
+    static let optimismTestnet = Network(
+        name: "Optimism Sepolia",
+        rpcURL: "https://opt-sepolia.g.alchemy.com/v2/",
+        explorerURL: "https://optimistic.etherscan.io"
+    )
+}
+
 class Web3Manager {
     
     static let sharedManager = Web3Manager()
@@ -21,9 +52,18 @@ class Web3Manager {
         }
     }
     
+    func getAlchemyKey() -> String? {
+        if let ApiKey = ProcessInfo.processInfo.environment["ALCHEMY_KEY"] {
+            return ApiKey
+        } else {
+            return nil
+        }
+    }
     
     
-    
+    func deployToken() {
+        
+    }
     
     
     
